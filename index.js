07dcb2fde21fe7d716aedbf18647b5074f4d46a1
André de Magalhães <andre.magalhaes@ehi.com>
2020-06-05 00:40:25 -0300

    dev: addition to list already working

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-items = new Map()
-list = document.getElementById("list")
-inputButton = document.getElementById("inputTodo")
-clearAllButton = document.getElementById("clearAllTodos")
-clearCheckedButton = document.getElementById("clearCheckedTodos")
+let items = new Map()
+
+const list = document.getElementById("list")
+const inputButton = document.getElementById("inputTodo")
+const clearAllButton = document.getElementById("clearAllTodos")
+const clearCheckedButton = document.getElementById("clearCheckedTodos")
 
 
-function addToDoToList(todoInfo) {
-    this.items.set(todoInfo)
-    checkItemList()
+function log(text) {
+    return console.log('* ' + text)
 }
 
-function checkItemList() {
+function addToList(todoInfo) {
+    items.set(items.size + 1, todoInfo.value)
+    checkItemListCount()
+    readItemList()
+    pushToItemList()
+}
 
+function checkItemListCount() {
+    return log('Items size: ' + items.size)
+}
+
+function readItems() {
+    let itemList = new Array();
+
+    for (counter = 1; counter <= items.size; counter++) {
+        itemList.push(items.get(counter))
+    }
+
+    log('List of Items: ' + itemList)
+    return itemList
+}
+
+function pushToItemList() {
+    var item = document.createElement('li')
+
+    item.appendChild(document.createTextNode(items.get(items.size)))
+    this.list.appendChild(item)
+
+    return list
+}
+
+function removeFromList(identifier) {
+    items.delete(identifier)
 }
